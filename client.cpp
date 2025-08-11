b4277b8f95a243263016467a6115b57a917df00f
leclerced <whyitssoimportant@bk.ru>
2025-08-12 01:24:12 +0300

    rewrite code for futures updates, need fix with cleanUp func

--- client.cpp
+++ client.cpp
@@ -9,29 +9,58 @@
 int main () {
     setlocale(LC_ALL, "ru");
 
-    std::ofstream outLog;
+    Client clientA;
+    return clientA.run();
+}
+
+//=============================================
+
+Client::Client() : clientSocket_(-1) {
     outLog.open(WLOG, std::ios::app);
     if (!outLog.is_open()) {
         outLog.close();
-        exit(EXIT_FAILURE);
+        return;
     }
     outLog << "----ПОДКЛЮЧЕНИЕ КЛИЕНТА----\n";
+}
 
-    int client_socket_ = socket(AF_INET, SOCK_STREAM, 0);
-    if (client_socket_ == -1) {
-        outLog << "Ошибка: не удалось создать сокет\n";
+Client::~Client() {
+    cleanUp();
+}
+
+int Client::run() {
+    if (!outLog.is_open()) {
         return -1;
     }
-    outLog << "Сокет создан\n";
 
+    if (!setupSocket()) { return -1; }
+    if (!connectClient()) { return -1; }
+
+    handleClient();
+    outLog << "----ЗАВЕРШЕНИЕ ПРОГРАММЫ----\n\n\n";
+    return 0;
+}
+
+bool Client::setupSocket() {
+    clientSocket_ = socket(AF_INET, SOCK_STREAM, 0);
+    if (clientSocket_ == -1) {
+        outLog << "Ошибка: не удалось создать сокет\n";
+        return false;
+    }
+    outLog << "Сокет создан\n";
+    
+    return true;
+}
+
+bool Client::connectClient() {
     sockaddr_in serverAddress;
     serverAddress.sin_family = AF_INET;
     serverAddress.sin_port = htons(HOST);
     serverAddress.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-    if (connect(client_socket_, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) < 0) {
+    if (connect(clientSocket_, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) < 0) {
         outLog << "Ошибка: не удалось подключиться к серверу\n";
-        return -1;
+        return false;
     }
     outLog << "Подключение к серверу установлено!\n";
 
@@ -39,6 +68,10 @@ int main () {
     std::cout << "Теперь вы можете отправлять сообщения\n";
     std::cout << "Команды: 'exit' или 'quit' для выхода\n";
 
+    return true;
+}
+
+bool Client::handleClient() {
     std::string message;
     while (true) {
         std::cout << "Клиент: ";
@@ -49,22 +82,32 @@ int main () {
             continue;
         }
 
-        if (send(client_socket_, message.c_str(), message.length(), 0) < 0) {
+        if (send(clientSocket_, message.c_str(), message.length(), 0) < 0) {
             outLog << "Ошибка отправки сообщения\n";
             break;
         }
 
         if (message == "exit" || message == "quit") {
-            send(client_socket_, message.c_str(), message.length(), 0);
+            send(clientSocket_, message.c_str(), message.length(), 0);
             outLog << "Выход из программы...\n";
             std::cout << "Выход из программы...\n";
-            close(client_socket_);
+            cleanUp();
             break;
         }
     }
 
-    outLog << "----ЗАВЕРШЕНИЕ ПРОГРАММЫ----\n\n\n";
-    outLog.close();
-    close(client_socket_);
-    return 0;
+    return true;
+}
+
+bool Client::cleanUp() {
+    if (clientSocket_ != -1) {
+        close(clientSocket_);
+        clientSocket_ = -1;
+    }
+
+    if (outLog.is_open()) {
+        outLog.close();
+    }
+
+    return true;
 }
--- server.h
+++ server.h
@@ -1,5 +1,5 @@
-#ifndef HEADER_H
-#define HEADER_H
+#ifndef SERVER_H
+#define SERVER_H
 
 #pragma once
 
@@ -8,10 +8,32 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <cstring>
 
 #define HOST 54000
 #define BUFFER 4096
 #define WLOG "/Users/macbook/new-mode/files/server_log.txt"
 #define WHISTORY "/Users/macbook/new-mode/files/history.txt"
 
-#endif //HEADER_H  
+class Server {
+private:
+    int socket_;
+    int client_fd;
+    sockaddr_in serverAddress;
+    std::ofstream outLog;
+    std::ofstream outHistory;
+
+    bool setupSocket();
+    bool bindSocket();
+    bool listenSocket();
+    bool acceptClient();
+    bool handleClient();
+    bool cleanUp();
+
+public:
+    Server();
+    int run();
+    ~Server();
+};
+
+#endif //SERVER_H  
